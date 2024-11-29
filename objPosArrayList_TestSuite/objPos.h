4d037da4e7031f42cab71890c31ff1f282ce1ba1
wangj840-fall2024 <wangj840@mcmaster.ca>
2024-11-29 14:35:25 -0500

    jessica

--- objPosArrayList_TestSuite/objPos.h
+++ objPosArrayList_TestSuite/objPos.h
@@ -19,7 +19,10 @@ class objPos
         
         // Respect the rule of six / minimum four
         // [TODO] Implement the missing special member functions to meet the minimum four rule
-        
+        objPos(const objPos &obpo); //copy constructor
+		objPos operator=(const objPos &obpo); //assignment operator
+        ~objPos(); //destructor
+
         void setObjPos(objPos o);        
         void setObjPos(int xPos, int yPos, char sym);  
 
